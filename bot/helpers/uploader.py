be5d123e309039174fa15ad890de34431d9c90b2
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-01-26 00:13:37 +0330

    Update uploader.py

--- bot/helpers/uploader.py
+++ bot/helpers/uploader.py
@@ -16,7 +16,21 @@ class Uploader:
         self.file = file
         self.title = title
         self.video_category = {
-            42: "Shorts",
+            1: "Film & Animation",
+            2: "Autos & Vehicles",
+            10: "Music",
+            15: "Pets & Animal",
+            17: "Sports",
+            19: "Travel & Events",
+            20: "Gaming",
+            22: "People & Blogs",
+            23: "Comedy",
+            24: "Entertainment",
+            25: "News & Politics",
+            26: "Howto & Style",
+            27: "Education",
+            28: "Science & Technology",
+            29: "Nonprofits & Activism",
         }
 
     async def start(self, progress: callable = None, *args) -> Tuple[bool, str]:
